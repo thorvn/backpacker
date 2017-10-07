3e5df1ee2b4d6a632b78ae1f6cc72b00281d0f5d
Ryan Angelo <cadagryanangelo@gmail.com>
2017-10-07 16:03:47 +0800

    resource routes for trips

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   end
 
   root 'home#index'
+  resources :trips
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

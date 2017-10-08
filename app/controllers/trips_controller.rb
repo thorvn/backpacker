031a57fa36c9c467ffe4f974c26f471b31ef755d
sabinahofmann <hofmann@dievision.de>
2017-10-08 10:41:39 +0200

    rspec and factory girl

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -4,4 +4,19 @@ class TripsController < ApplicationController
 
   end
 
+  def available_trips
+    trips = Trip.available_trips
+    trips = trips.pricing(trip_params[:price])
+    trips = trips.range_of_time(trip_params[:start_date], trip_params[:end_date])
+    trips = trips.members(trip_params[:number_of_member])
+
+    render json: trips
+  end
+
+  private
+
+  def trip_params
+
+  end
+
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,10 @@
 class Trip < ApplicationRecord
+
+  scope :available_trips, -> { where("start_date >= ? and user_id != ?", Date.today, current_user) }
+  scope :pricing, ->(price) { where("price <= ?", price) }
+  scope :range_of_time, ->(start_date, end_date) do
+    where("start_date = ? and end_date = ?", start_date, end_date)
+  end
+
+  scope :members, ->(number) { where("number_of_member = ?", number_of_member) }
 end
